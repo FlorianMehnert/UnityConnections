b89068edf372d1ab3dc7a15d39575adb65494752
Florian Mehnert <florian.mehnert@gmail.com>
2025-08-30 18:11:09 +0200

    add some events for testing purposes

---
+++ Assets/Scripts/CollisionEvents.cs
@@ -0,0 +1,12 @@
+﻿using FxResources.System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CollisionEvents : MonoBehaviour
+{
+    [SerializeField] private UnityEvent onTriggerEnter;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        onTriggerEnter.Invoke();
+    }
+}
